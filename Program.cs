ac3159851826bbd7387d0ce8b3cebb2b6e630346
= <=>
2021-06-24 14:09:22 -0300

    Fim exercício HoraMaisTarde

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MaisTarde
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            DateTime agora = DateTime.Now;
+            TimeSpan SeisHorasEMeia = new TimeSpan(6, 30, 0);
+            
+            DateTime Resultado = agora + SeisHorasEMeia;
+
+            Console.WriteLine($" {Resultado}");
+        }
+    }
+}
